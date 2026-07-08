da313a8e17bda92240adc740b12381268fdd8ba2
Mohamed Magdy El-Yemeni <mm6941033@gmail.com>
2026-07-08 12:32:24 +0300

    Add SetAvailabilityCommandValidator for command validation

---
+++ backend(.net)/BayTack/BayTack.Application/Features/Providers/Commands/SetAvailability/SetAvailabilityCommandValidator.cs
@@ -0,0 +1,16 @@
+using FluentValidation;
+
+namespace BayTack.Application.Features.Providers.Commands.SetAvailability
+{
+	public sealed class SetAvailabilityCommandValidator : AbstractValidator<SetAvailabilityCommand>
+	{
+		public SetAvailabilityCommandValidator()
+		{
+			RuleFor(x => x.ProviderProfileId).NotEmpty();
+			RuleFor(x => x.DayOfWeek).IsInEnum();
+			RuleFor(x => x)
+				.Must(x => x.StartTime < x.EndTime)
+				.WithMessage("Start time must be before end time.");
+		}
+	}
+}
