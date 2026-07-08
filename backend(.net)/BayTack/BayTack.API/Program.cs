71e67d31757a85c2ca6e41e25a6196233b5f7b32
mahmoud <mahmoud.mahmed.salah84@gmail.com>
2026-07-08 05:23:05 +0300

    add refence for infra in Api

--- backend(.net)/BayTack/BayTack.API/Program.cs
+++ backend(.net)/BayTack/BayTack.API/Program.cs
@@ -1,3 +1,8 @@
+
+
+
+using BayTack.Infrastructure;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -5,6 +10,15 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+
+
+builder.Services.AddInfrastructure(builder.Configuration);
+
+
+
+
+
+
 
 var app = builder.Build();
 
---
+++ backend(.net)/BayTack/BayTack.Infrastructure/DependencyInjection.cs
@@ -0,0 +1,51 @@
+﻿using BayTack.Application.Abstractions.Interfaces;
+using BayTack.Application.Abstractions.IRepository;
+using BayTack.Infrastructure.Identity;
+using BayTack.Infrastructure.Persistence;
+using BayTack.Infrastructure.Repositorty;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+
+namespace BayTack.Infrastructure
+{
+	public static class DependencyInjection
+	{
+		public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
+		{
+
+
+
+			//services.AddDbContext<AppDbContext>((sp, options) =>
+			//{
+
+			//	options.UseSqlServer(
+			//		configuration.GetConnectionString("DefaultConnection"),
+			//		sql => sql.EnableRetryOnFailure());
+			//});
+
+
+
+
+			//// Identity DB (separate)
+			//services.AddJobSiteIdentity(configuration);
+
+			// Repositories
+			//services.AddScoped(typeof(IRepository<>), typeof(Repository<>));
+			//services.AddScoped<IUserRepository, UserRepository>();
+			//services.AddScoped<ICompanyRepository, CompanyRepository>();
+			//services.AddScoped<IJobRepository, JobRepository>();
+			//services.AddScoped<IApplicationRepository, ApplicationRepository>();
+			//services.AddScoped<ICVRepository, CVRepository>();
+			//services.AddScoped<ISkillRepository, SkillRepository>();
+			//services.AddScoped<ICVJobRecommendationRepository, CVJobRecommendationRepository>();
+			//services.AddScoped<IUserSkillRepository, UserSkillsRepository>();
+
+
+			// Unit of Work
+			services.AddScoped<IUnitOfWork, UnitOfWork>();
+
+			return services;
+		}
+	}
+}
