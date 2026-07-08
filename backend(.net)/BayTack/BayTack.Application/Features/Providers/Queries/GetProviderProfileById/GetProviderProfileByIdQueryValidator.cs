0006b8a6345f05656ef0fc96c1177228f7185948
Mohamed Magdy El-Yemeni <mm6941033@gmail.com>
2026-07-08 03:24:02 +0300

    Add validator for GetProviderProfileById query

---
+++ backend(.net)/BayTack/BayTack.Application/Features/Providers/Queries/GetProviderProfileById/GetProviderProfileByIdQueryValidator.cs
@@ -0,0 +1,12 @@
+using FluentValidation;
+
+namespace BayTack.Application.Features.Providers.Queries.GetProviderProfileById
+{
+	public sealed class GetProviderProfileByIdQueryValidator : AbstractValidator<GetProviderProfileByIdQuery>
+	{
+		public GetProviderProfileByIdQueryValidator()
+		{
+			RuleFor(x => x.ProviderProfileId).NotEmpty();
+		}
+	}
+}
