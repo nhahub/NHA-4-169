fc94549f2392f26fd144aa848cdf3ad35ec7daf1
Mohamed Magdy El-Yemeni <mm6941033@gmail.com>
2026-07-09 21:59:50 +0300

    Add specification for provider profile by portfolio item ID

---
+++ backend(.net)/BayTack/BayTack.Application/Features/Providers/Commands/UpdatePortfolioItem/ProviderProfileByPortfolioItemIdSpec.cs
@@ -0,0 +1,14 @@
+using BayTack.Application.Common.Specifications;
+using BayTack.Domain.Entities.ProviderAggregate;
+
+namespace BayTack.Application.Features.Providers.Commands.UpdatePortfolioItem
+{
+	public sealed class ProviderProfileByPortfolioItemIdSpec : Specification<ProviderProfile>
+	{
+		public ProviderProfileByPortfolioItemIdSpec(string itemId) : base(p => p.Portfolio.Any(i => i.Id == itemId))
+		{
+			AddInclude(p => p.Portfolio);
+			EnableTracking();
+		}
+	}
+}
