4029727b00f95d825c0c5556d964fdfe0434a3c4
Mohamed Magdy El-Yemeni <mm6941033@gmail.com>
2026-07-11 14:19:18 +0300

    Add ProviderResponse and Respond method to Review

--- backend(.net)/BayTack/BayTack.Domain/Entities/Review.cs
+++ backend(.net)/BayTack/BayTack.Domain/Entities/Review.cs
@@ -1,8 +1,7 @@
-﻿using BayTack.Domain.Common.BaseEntity;
+using BayTack.Domain.Common.BaseEntity;
 using System;
 using System.Collections.Generic;
 using System.Text;
-
 namespace BayTack.Domain.Entities
 {
 	public class Review : BaseEntity<string>
@@ -11,15 +10,13 @@ namespace BayTack.Domain.Entities
 		public string CustomerId { get; private set; }
 		public int Rating { get; private set; }
 		public string? ReviewText { get; private set; }
+		public string? ProviderResponse { get; private set; }
 		public DateTime CreatedAt { get; private set; } = DateTime.UtcNow;
-
 		private Review() { }
-
 		public static Review Create(string orderId, string customerId, int rating, string? reviewText)
 		{
 			if (rating is < 1 or > 5)
 				throw new ArgumentOutOfRangeException(nameof(rating), "Rating must be between 1 and 5.");
-
 			return new Review
 			{
 				Id = Guid.NewGuid().ToString(),
@@ -29,6 +26,12 @@ namespace BayTack.Domain.Entities
 				ReviewText = reviewText
 			};
 		}
-	}
 
+		public void Respond(string responseText)
+		{
+			if (string.IsNullOrWhiteSpace(responseText))
+				throw new ArgumentException("Response text is required.", nameof(responseText));
+			ProviderResponse = responseText;
+		}
+	}
 }
