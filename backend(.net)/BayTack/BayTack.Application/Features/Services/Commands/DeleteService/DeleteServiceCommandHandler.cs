b0d6a2acc87739aa97d88df99d75c5ff72b2b7a7
mahmoud <mahmoud.mahmed.salah84@gmail.com>
2026-09-12 11:30:17 +0300

    Add CRUD for Service entities, update .gitignore
    Introduce ServicesController with create/delete endpoints using MediatR and DTOs. Implement CreateServiceCommand, handler, and validator with category and payment method checks. Add DeleteServiceCommand and handler. Update .gitignore for API logs. Comment out authorization in CategoriesController for create action.

---
+++ backend(.net)/BayTack/BayTack.Application/Features/Services/Commands/DeleteService/DeleteServiceCommandHandler.cs
@@ -0,0 +1,29 @@
+﻿using BayTack.Application.Abstractions.IRepository;
+using BayTack.Application.Abstractions.Messaging;
+using BayTack.Application.Common.Models;
+using BayTack.Domain.Entities.ServiceAggregate;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BayTack.Application.Features.Services.Commands.DeleteService
+{
+	public sealed class DeleteServiceCommandHandler : ICommandHandler<DeleteServiceCommand>
+	{
+		private readonly IRepository<Service, string> _serviceRepository;
+		public DeleteServiceCommandHandler(
+			IRepository<Service, string> serviceRepository )
+		{
+			_serviceRepository = serviceRepository;
+		}
+		public async Task<Result> Handle(DeleteServiceCommand request, CancellationToken ct)
+		{
+			var service = await _serviceRepository.GetByIdAsync(request.Id, ct);
+			if (service is null)
+				return Result.Failure("Service not found.");
+			_serviceRepository.Remove(service);
+
+			return Result.Success();
+		}
+	}
+}
